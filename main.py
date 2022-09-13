095e8dfe3740e95790c340537afb0087c4a552fd
seran sivananthamoorthy <5sivas01@solent.ac.uk>
2022-09-12 22:38:41 -0500

    Add main option menu

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 
 def main_menu():
-    pass
+    print("Welcome to BillBoard 100, Please select the option from below menu")
+    print("""
+    1. top ranked song of the day
+    2. artist who has most top ranked songs
+    3. 10 songs which had longest weeks on the board
+    4. song which has moved most ranks
+    5. visualize top songs
+    6. exit
+    """)
+    inp = input("please select the option: ")
+    print("showing option " + inp)
+    i = int(inp)
+    if i == 1:
+        pass
+    else:
+        print("thanks for choosing our service")
+        exit(1)
+
 
 if __name__ == '__main__':
     main_menu()
